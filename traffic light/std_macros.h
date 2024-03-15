86841eb582159f72c58eb9527b9ce89289ce412a
Shawky Ramadan <161169216+ShawkyRamadan@users.noreply.github.com>
2024-03-15 16:03:46 +0200

    Add files via upload

---
+++ traffic light/std_macros.h
@@ -0,0 +1,15 @@
+/*
+ * std_macros.h
+ *
+ * Created: 2/6/2023 4:06:22 PM
+ *  Author: Shawky Ramadan
+ */
+#define REGISTER_SIZE 8
+#define SET_BIT(reg,bit)    reg|=(1<<bit)
+#define CLR_BIT(reg,bit)    reg&=(~(1<<bit))
+#define TOG_BIT(reg,bit)    reg^=(1<<bit)
+#define READ_BIT(reg,bit)    (reg&(1<<bit))>>bit
+#define IS_BIT_SET(reg,bit)  (reg&(1<<bit))>>bit
+#define IS_BIT_CLR(reg,bit)  !((reg&(1<<bit))>>bit)
+#define ROR(reg,num)         reg=(reg<<(REGISTER_SIZE-num))|(reg>>(num))
+#define ROL(reg,num)        reg= (reg>>(REGISTER_SIZE-num))|(reg<<(num))
